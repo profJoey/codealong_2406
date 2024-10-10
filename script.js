9ed79797729ba8441830e7502abccd1ce06fd789
Joey Azoulai <joey.azoulai@nyu.edu>
2024-10-10 11:28:46 -0400

    completed version

---
+++ script.js
@@ -0,0 +1,10 @@
+function addItem() {
+    const input = document.getElementById('itemInput');
+    const itemText = input.value.trim();
+    if (itemText !== "") {
+        const li = document.createElement('li');
+        li.textContent = itemText;
+        document.getElementById('itemList').appendChild(li);
+        input.value = "";
+    }
+}
